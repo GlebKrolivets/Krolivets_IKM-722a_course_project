9b72416a4f0180a0657034ce8712a2f17d378425
Гліб Кролівець <Hlib.Krolivets@infiz.khpi.edu.ua>
2024-06-05 13:20:37 +0300

    Finish Task number 1

--- Krolivets_IKM-722a_course_project/Form1.cs
+++ Krolivets_IKM-722a_course_project/Form1.cs
@@ -12,9 +12,58 @@ namespace Krolivets_IKM_722a_course_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode;//Режим дозвілу або заборони
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага!");//Виведення повідомлення на екран
+            tClock.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if(Mode)
+            {
+                tbInput.Enabled = true;//режим дозволу введення
+                tbInput.Focus();
+                tClock.Start();
+                bStart.Text="Стоп";//зміна тексту на кнопці на Стоп
+                this.Mode = false;
+            }
+            else
+            {
+                tbInput.Enabled = false;//режим заборони введення
+                tClock.Stop();
+                bStart.Text="Пуск";////зміна тексту на кнопці на Пуск
+                this.Mode=true;
+            }
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar>='0') &(e.KeyChar <= '9') |(e.KeyChar ==(char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Некоректний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar=(char)0;
+            }
         }
     }
 }
